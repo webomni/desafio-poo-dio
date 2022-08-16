131af20e8e48b27b359d796171c2d899859e5c47
PauloHcastro <prpaulohcmg@gmail.com>
2022-08-16 10:30:41 -0300

    Mentoria e Curso

--- desafio-poo-dio/src/App.java
+++ desafio-poo-dio/src/App.java
@@ -1,5 +1,33 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        try {
+            Curso curso1 = new Curso();
+            curso1.setTitulo("Curso java");
+            curso1.setDescricao("Descrição do curso");
+            curso1.setCargaHoraria(8);
+
+            Mentoria mentoria = new Mentoria();
+            mentoria.setTitulo("Mentoria Java");
+            mentoria.setDescricao("Descrição mentoria Java");
+            mentoria.setData(LocalDate.now());
+
+            System.out.println(curso1.getTitulo());
+
+            // muito interesane uso de pattern para formatar data
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            System.out.println(mentoria.getDescricao() + " - " + mentoria.getTitulo() + " - "
+                    + mentoria.getData().format(formatter));
+
+        } catch (Exception e) {
+            System.err.println("Houve um erro !" + e.getMessage());
+        }
+
     }
 }
